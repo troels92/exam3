bce4c37150c129ec776728e84f5d29327092bc26
Troels Smidt <Razorsmidt@hotmail.com>
2020-12-21 16:57:02 +0100

    Only missing ManyToOne instead of static data on supervisors

--- src/main/java/com/exam/ts/controller/SupervisorController.java
+++ src/main/java/com/exam/ts/controller/SupervisorController.java
@@ -6,40 +6,64 @@ import com.exam.ts.repository.StudentRepository;
 import com.exam.ts.repository.SupervisorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+import java.util.Optional;
+@RestController
 public class SupervisorController {
-/*
+
+
     @Autowired
     SupervisorRepository supervisorRepository;
 
+
+
+    //Show index
+    @GetMapping("/supervisor")
+    public Iterable<Supervisor> index(){
+        return supervisorRepository.findAll();
+    }
+
+    //get by id
+    @GetMapping("/supervisor/{id}")
+    public ResponseEntity<Optional<Supervisor>> readOne(@PathVariable Long id){
+
+        Optional<Supervisor> res = supervisorRepository.findById(id);
+        if(res.isPresent()){
+            return ResponseEntity.status(200).body(res);
+        }else{
+            return ResponseEntity.status(404).body(res);
+        }
+    }
+
+
     //post
     @CrossOrigin(origins = "*", exposedHeaders = "Location")
-    @PostMapping("/student")
+    @PostMapping("/supervisor")
     public ResponseEntity<String> create(@ModelAttribute Supervisor su) {
         Supervisor supervisor = supervisorRepository.save(su);
 
-        return ResponseEntity.status(201).header("Location", "/student/" + supervisor.getId()).body("{'Msg': 'post created'}");
+        return ResponseEntity.status(201).header("Location", "/supervisor/" + supervisor.getId()).body("{'Msg': 'post created'}");
     }
 
 
     //put
     @CrossOrigin(origins = "*", exposedHeaders = "Location")
-    @PostMapping("/student/{id}")
+    @PostMapping("/supervisor/{id}")
     public ResponseEntity <String> update(@ModelAttribute Supervisor su){
         Supervisor supervisor = supervisorRepository.save(su);
 
-        return ResponseEntity.status(201).header("Location", "/student/" + supervisor.getId()).body("{'Msg': 'post created'}");
+        return ResponseEntity.status(201).header("Location", "/supervisor/" + supervisor.getId()).body("{'Msg': 'post created'}");
     }
 
     //delete
     @CrossOrigin(origins = "*")
-    @DeleteMapping("/student/{id}")
+    @DeleteMapping("/supervisor/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id){
         supervisorRepository.deleteById(id);
         return ResponseEntity.status(200).body("{'msg':'Deleted'}");
     }
-*/
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,6 @@
-/*
+
 insert into supervisor (id, name) values (1, 'Lars');
 insert into supervisor (id, name) values (2, 'Niels');
-*/
 
 insert into student (id, name, email, supervisor_id) values (1, 'Bent Olsen', 'bent74@stud.kea.dk', 1);
 insert into student (id, name, email, supervisor_id) values (2, 'Fie Hansen', 'fie54@stud.kea.dk', 1);
